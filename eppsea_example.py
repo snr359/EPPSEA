e829a894dbb60c91a3e65ffe414124eab0a5844e
snr359 <snr359@mst.edu>
2018-02-08 20:34:22 -0600

    Update eppsea_example

--- eppsea_example.py
+++ eppsea_example.py
@@ -4,8 +4,8 @@ import eppsea_base
 
 class example_evaluator:
     def evaluate(self, selection_function):
-        return random.random()
+        return {'fitness':random.random()}
 
 if __name__ == '__main__':
     new_evaluator = example_evaluator()
-    eppsea_base.eppsea(new_evaluator, 'config/base_config/test.cfg')
+    eppsea_base.eppsea(new_evaluator, 'config/base_config/basicEA.cfg')
