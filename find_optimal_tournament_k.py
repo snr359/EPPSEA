a953fd31ddd3bd0329444ca99b8217c0e5641e6a
Samuel Richter <snr359@mst.edu>
2018-03-22 16:41:33 -0500

    Improved tournament k finder script performance

--- find_optimal_tournament_k.py
+++ find_optimal_tournament_k.py
@@ -3,39 +3,57 @@ import sys
 import configparser
 import multiprocessing
 import statistics
+import math
 
 import eppsea_basicEA
 
 def test_k_tournament(basic_ea, k):
     # tests a value for k tournament selection and returns the average best fitness of the EA
-    results = []
     basic_ea.tournament_k = k
+    params = []
     for _ in range(basic_ea.runs):
-        results.append(basic_ea.one_run('k_tournament', None))
+        params.append(('k_tournament', None))
+
+    pool = multiprocessing.Pool()
+    results = pool.starmap(basic_ea.one_run, params)
+    pool.close()
 
     average_best = statistics.mean(r['final_best_fitness'] for r in results)
 
     return average_best
 
-def find_optimal_k(basic_ea, num_processes):
+def find_optimal_k(basic_ea, num_k_values=None):
+    # if num_processes is None or invalid, approximate a value for a two-iteration approach
+    if num_k_values is None or num_k_values < 4:
+        num_k_values = max(4, math.ceil(math.sqrt(2 * basic_ea.mu)))
+
+    # save old k-tournament value so it can be reassigned later
+    old_tournament_k = basic_ea.tournament_k
 
     # sample for an interval for the optimum value by sectioning (assuming the optimal k-value is a local optimum among all possible k values)
     min_k = 1
     max_k = basic_ea.mu
+    known_values = dict()
 
-    while (max_k - min_k)+1 > num_processes:
+    while (max_k - min_k)+1 > num_k_values:
         # determine evenly spaced k values
-        stepsize = (max_k - min_k) / (num_processes - 1)
+        stepsize = (max_k - min_k) / (num_k_values - 1)
         k_values = []
-        for i in range(num_processes):
+        for i in range(num_k_values - 1):
             k_values.append(round(min_k + i*stepsize))
+        k_values.append(max_k)
 
         # test the k values
         print('Testing with k-values: {0}'.format(k_values))
-        pool = multiprocessing.Pool(processes=num_processes)
-        params = list((basic_ea, k) for k in k_values)
-        results = pool.starmap(test_k_tournament, params)
-        pool.close()
+        results = []
+        for k in k_values:
+            if k in known_values:
+                result = known_values[k]
+            else:
+                result = test_k_tournament(basic_ea, k)
+
+            results.append(result)
+            known_values[k] = result
 
         # if the first value is the maximum, set min and max to the first and second values
         if results[0] == max(results):
@@ -58,18 +76,30 @@ def find_optimal_k(basic_ea, num_processes):
     # once there is a small interval for optimal k values, test every value in that interval
     k_values = list(range(min_k, max_k+1))
     print('Testing with k-values: {0}'.format(k_values))
-    pool = multiprocessing.Pool(processes=num_processes)
-    params = list((basic_ea, k) for k in k_values)
-    results = pool.starmap(test_k_tournament, params)
-    pool.close()
+    results = []
+    for k in k_values:
+        if k in known_values:
+            result = known_values[k]
+        else:
+            result = test_k_tournament(basic_ea, k)
+
+        results.append(result)
+        known_values[k] = result
 
     # calculate the best k values
     top = results.index(max(results))
+
+    # re-set old k-tournament value before returning
+    basic_ea.tournament_k = old_tournament_k
+
     return k_values[top]
 
 if __name__ == '__main__':
 
-    num_processes = int(sys.argv[2])
+    if len(sys.argv) < 3:
+        num_k_values = None
+    else:
+        num_k_values = int(sys.argv[2])
 
     if len(sys.argv) < 2:
         print('Please provide config file')
@@ -83,6 +113,6 @@ if __name__ == '__main__':
     # create a basic EA object to do the evaluations with
     basic_ea = eppsea_basicEA.basicEA(config)
 
-    optimal_k = find_optimal_k(basic_ea, num_processes)
+    optimal_k = find_optimal_k(basic_ea, num_k_values)
 
     print('Best k tournament value: {0}'.format(optimal_k))
