c8acf4e4745fe0e5633836e006af2c7ac527c069
Samuel Richter <snr359@mst.edu>
2018-11-14 18:14:16 -0600

    Update test script

--- interactive_cmaes_test.py
+++ interactive_cmaes_test.py
@@ -1,6 +1,7 @@
 import sys
 import configparser
 import statistics
+import os
 
 import eppsea_base
 import eppsea_cmaes
@@ -13,6 +14,16 @@ def log(s):
         file.write('{0}\n'.format(s))
     
 def main(config_path):
+    if os.path.isfile('output.txt'):
+        print('output.txt already exists. Remove? (y/n)')
+        while True:
+            user_input = input()
+            if user_input == 'y':
+                os.remove('output.txt')
+                break
+            elif user_input == 'n':
+                break
+
     config = configparser.ConfigParser()
     config.read(config_path)
 
@@ -39,13 +50,12 @@ def main(config_path):
         if user_input == 's':
             break
 
-    if len(eppsea_selection_functions) > 0:
-        new_selection_function = eppsea_base.EppseaSelectionFunction(eppsea_selection_functions[0])
-        new_selection_function.gp_trees[0].root.operation = 'fitness'
-        new_selection_function.gp_trees[0].root.children = []
-        new_selection_function.gp_trees[0].selection_type = 'truncation'
+    new_selection_function = eppsea_base.EppseaSelectionFunction(eppsea.population[0])
+    new_selection_function.gp_trees[0].root.operation = 'fitness'
+    new_selection_function.gp_trees[0].root.children = []
+    new_selection_function.gp_trees[0].selection_type = 'truncation'
 
-        eppsea_selection_functions.append(new_selection_function)
+    eppsea_selection_functions.append(new_selection_function)
 
     selection_functions = []
     for e in eppsea_selection_functions:
@@ -63,10 +73,10 @@ def main(config_path):
 
     all_results = ea_results + basic_cmaess_results
 
-    basic_average_best_fitnesses = dict()
+    basic_median_best_fitnesses = dict()
     for f in fitness_functions:
         f_results = list(r for r in basic_cmaess_results if r.fitness_function_id == f.id)
-        basic_average_best_fitnesses[f.id] = statistics.mean(r.final_best_fitness for r in f_results)
+        basic_median_best_fitnesses[f.id] = statistics.median(r.final_best_fitness for r in f_results)
 
     for f in fitness_functions:
         log('Results on fitness function {0} with id {1}'.format(f.display_name, f.id))
@@ -74,7 +84,7 @@ def main(config_path):
         for s in selection_functions:
             log('\tResults for EPPSEA member: {0}'.format(s.eppsea_selection_function.get_string()))
             s_results = list(r for r in f_results if r.selection_function_id == s.id)
-            percentage_better = len(list(r for r in s_results if r.final_best_fitness <= basic_average_best_fitnesses[f.id])) / len(s_results)
+            percentage_better = len(list(r for r in s_results if r.final_best_fitness <= basic_median_best_fitnesses[f.id])) / len(s_results)
             percentage_better = round(100 * percentage_better, 2)
             log('\t\tThis selection function beat basic cmaes {0}% of the time'.format(percentage_better))
 
@@ -82,10 +92,10 @@ def main(config_path):
         if i < cmaes.testing_runs:
             log('\n----------------------------------------------------------------------------\n')
             log('Same reports if only the first {0} results are taken:'.format(i))
-            basic_average_best_fitnesses = dict()
+            basic_median_best_fitnesses = dict()
             for f in fitness_functions:
                 f_results = list(r for r in basic_cmaess_results if r.fitness_function_id == f.id)[:i]
-                basic_average_best_fitnesses[f.id] = statistics.mean(r.final_best_fitness for r in f_results)
+                basic_median_best_fitnesses[f.id] = statistics.mean(r.final_best_fitness for r in f_results)
 
             for f in fitness_functions:
                 log('Results on fitness function {0} with id {1}'.format(f.display_name, f.id))
@@ -93,7 +103,7 @@ def main(config_path):
                 for s in selection_functions:
                     log('\tResults for EPPSEA member: {0}'.format(s.eppsea_selection_function.get_string()))
                     s_results = list(r for r in f_results if r.selection_function_id == s.id)[:i]
-                    percentage_better = len(list(r for r in s_results if r.final_best_fitness <= basic_average_best_fitnesses[f.id])) / len(s_results)
+                    percentage_better = len(list(r for r in s_results if r.final_best_fitness <= basic_median_best_fitnesses[f.id])) / len(s_results)
                     percentage_better = round(100 * percentage_better, 2)
                     log('\t\tThis selection function beat basic cmaes {0}% of the time'.format(percentage_better))
 
